a4c8680a785404e2ea37e96eccae40adc745cdeb
Mike Long <mlong@pivotal.io>
2016-06-15 17:01:21 -0700

    adding some index pages

--- app/controllers/proto_controller.rb
+++ app/controllers/proto_controller.rb
@@ -1,4 +1,10 @@
 class ProtoController < ApplicationController
   def index
   end
+  def install
+  end
+  def use
+  end
+  def extend
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'proto/index'
+  get 'proto/index' => 'proto#index'
+  get 'install' => 'proto#install'
+  get 'use' => 'proto#use'
+  get 'extend' => 'proto#extend'
   root 'proto#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
